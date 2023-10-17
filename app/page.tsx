25a684fd655106eeb5ed2d15a3ad33e7501a6f47
LionAB <lionelabatan@gmail.com>
2023-10-17 17:09:43 +0200

    Build NavBar

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div><h1 className="text-4xl font-bold text-center">Hello World</h1></div>
+      <div><h1>Hello World</h1></div>
     </main>
   )
 }
