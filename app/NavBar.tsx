250ae243bb8cc71c4b696dbafc1ccea8a5d036ef
LionAB <lionelabatan@gmail.com>
2023-10-17 17:23:21 +0200

    Style lien actif NavBar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,7 +1,11 @@
+'use client'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 import { VscDebugAlt } from 'react-icons/vsc'
+import classnames from 'classnames';
 const NavBar = () => {
+    const currentPath = usePathname();
     const links= [
         {label :'Dashboard', href:'/'},
         {label :'Issues', href:'/issues'},
@@ -10,8 +14,15 @@ const NavBar = () => {
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><VscDebugAlt/></Link>
         <ul className='flex space-x-6'>
-            {links.map(link=><Link key={link.href} className='text-zinc-500 hover:text-zinc-800' href={link.href}>{link.label}</Link>)}
-           
+            {links.map(link=>
+            <Link 
+            key={link.href} 
+            className={classnames({
+                'text-zinc-900': link.href===currentPath,
+                'text-gray-500': link.href!==currentPath,
+                'hover:text-zinc-800 transition-colors':true
+            })}
+            href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
 
